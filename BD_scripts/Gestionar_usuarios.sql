c61cd30231d92ff32c286685638f4e0c063e91da
Jerlinson Gonzalez <jerlinsongonzalez0000@gmail.com>
2024-09-29 20:51:25 -0400

    Primeros scripts agregados

---
+++ BD_scripts/Gestionar_usuarios.sql
@@ -0,0 +1,7 @@
+
+/*
+La creacion de un usuario se compone de dos partes,
+'nombre_usuario'@'nombre_servidor' y la contrasenia o 'IDENTIFIED'
+*/
+
+CREATE USER IF NOT EXISTS 'jerlinson1'@'localhost' IDENTIFIED BY '1234'
