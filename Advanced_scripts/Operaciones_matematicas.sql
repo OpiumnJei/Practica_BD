e5813933ab154f81aec493798060151077998c73
Jerlinson Gonzalez <jerlinsongonzalez0000@gmail.com>
2024-10-06 15:45:43 -0400

    arithmetics operations

---
+++ Advanced_scripts/Operaciones_matematicas.sql
@@ -0,0 +1,13 @@
+-- OPERACIONES ARITMETICAS 
+SELECT 5 + 8 AS calculo;
+SELECT 5 - 8 AS calculo;
+SELECT 5 * 8 AS calculo;
+SELECT 5 / 8 AS calculo;
+
+-- FUNCIONES MATEMATICAS
+SELECT MOD(5,4); -- calcular residuo o modulo
+SELECT CEILING(9.5); -- redondea un decimal arriba(ceiling = techo)
+SELECT FLOOR(9.6);-- redondea un decimal abajo
+SELECT ROUND(9.7); -- redondea al entero mas cercano
+SELECT POW(5,3); -- elevar un numero a la potencia
+SELECT SQRT(9); -- sacar la raiz cuadrada a un numero
